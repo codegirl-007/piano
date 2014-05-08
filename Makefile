04e0dc8242713735f7a7a16de706d4a62be6ca56
Stephanie Chung <schung@linkedin.com>
2014-05-07 19:18:16 -0700

    - committing sound files - added functionality to play sound on click - work in progress: key bindings.

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 commonjs:
 	@node_modules/requirejs/bin/r.js -convert js/src js/app/
 	@echo "JS Compiled."
+
+watch-css:
+	@compass watch
+	@echo "Watching you..."
--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
+var wholeStep = 2,
+  halfStep = 1;
+
+var major3rd = (2 * wholeStep);
+var major5th = major3rd + (3 * halfStep);
+//minor chord
+
 window.onload = function () {
   MIDI.loadPlugin({
     soundfontUrl: "./soundfont/",
     instrument: "acoustic_grand_piano",
     callback: function() {
-      $('#piano').click(function(event) {
+      $('#piano').on('click', function(event) {
         var note = $(event.target).data('note');
-
-        playNote(note)
+        playNote(note);
+      });
+      $(document).keydown(function(event) {
+        var keycode = event.keyCode;
+        if (keycode == 81) {
+          $('.anchor[data=60]').trigger('click');
+          console.log('click');
+        }
       });
     }
   });
 
 };
 
-function pianoReady() {
-
-}
-
 function playNote(note) {
+  console.log(note);
   var delay = 0; // play one note every quarter second
   var velocity = 127; // how hard the note hits
   MIDI.setVolume(0, 127);
